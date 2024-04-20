49c4499fff19b90251619027a358b7f0de37bd00
Munawir Fikri <munawirfikri@gmail.com>
2024-04-20 14:34:28 +0700

    initial commit

---
+++ src/mock/data.js
@@ -0,0 +1,53 @@
+export default {
+  code: 
+`
+/* Halo semua!
+Jangan panik, ini bukan tampilan hacking.
+Ini hanyalah tampilan undangan Vinni & Fikri */
+
+const { 😆 } = Fikri;
+const { 🤗 } = Vinni;
+😆.say('Capek juga ldr terus yah ❤️');
+🤗.response('Iyaah！');
+
+// Kita pun sepakat untuk menikah
+// Lamaran dah kite lakukan tgl 14 April 2024.`,
+  executions: [{
+    name:'Merisik',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'Berunding',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'Tunangan',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'Pre-wedding',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'Poto biru',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'Ngurus KUA 😄',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'Upcoming untuk Akad dan Resepsi...',
+    time: '',
+    visible: false
+  }],
+  barrages: [
+    '66666666',
+  ],
+}
